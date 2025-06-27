9856d57071b88fad49654d07806fa27e347c223f
Jayaprakash-008 <jarvisjp008@gmail.com>
2025-06-27 23:02:20 +0530

    update

--- index.py
+++ index.py
@@ -1,1 +1,17 @@
-print("hello")
+"""Variable = value"""
+id=141300119
+department="quality department"
+age=25
+salary=18.500
+pf=True
+print(id)
+print(department)
+print(age)
+print(salary)
+print(pf)
+"""Finding the type of variables"""
+print(type(id))
+print(type(department))
+print(type(age))
+print(type(salary))
+print(type(pf))
