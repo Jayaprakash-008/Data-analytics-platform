dc3fef88db17b1592955941ad7708ab0bd646098
Jayaprakash-008 <jarvisjp008@gmail.com>
2025-06-28 18:31:06 +0530

    update

--- index.py
+++ index.py
@@ -1,16 +1,19 @@
 """Variable = value"""
 id=141300119
+Id=55045
 department="quality department"
 age=25
-salary=18.500
-pf=True
+salary=20000
+pf=True #provident fund
 print(id)
+print(Id)
 print(department)
 print(age)
 print(salary)
 print(pf)
 """Finding the type of variables"""
 print(type(id))
+print((type(Id)))
 print(type(department))
 print(type(age))
 print(type(salary))
