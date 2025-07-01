d89655a75f479088e7e64be037d41916dc881fec
Jayaprakash-008 <jarvisjp008@gmail.com>
2025-07-01 22:15:26 +0530

    create functions

---
+++ functions.py
@@ -0,0 +1,27 @@
+def addition(number1,number2,number3):
+    ans = number1 + number2 + number3
+    return ans
+
+def subtraction(number1,number2,number3):
+    ans = number1 - number2 - number3
+    return ans
+
+def multiplication(number1,number2,number3):
+    ans = number1 * number2 * number3
+    return ans
+
+def division(number1,number2,number3):
+    ans = number1 / number2 / number3
+    return ans
+
+ans = addition(10,20,30)
+print(ans)
+
+ans = subtraction(10,20,30)
+print(ans)
+
+ans = multiplication(10,20,30)
+print(ans)
+
+ans = division(10,20,30)
+print(ans)
